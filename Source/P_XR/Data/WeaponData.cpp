7fb25cb2db67a49f68adbf217db44e16287cfef4
groun519 <groun519@gmail.com>
2025-06-09 01:15:15 +0900

    goood6

--- Source/P_XR/Data/WeaponData.cpp
+++ Source/P_XR/Data/WeaponData.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "P_XR/Data/WeaponData.h"
+#include "WeaponData.h"
 
 FWeaponInfo UWeaponData::FindWeaponForWeaponName(FName WeaponName)
 {
